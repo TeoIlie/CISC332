868d34fc4764641f8272c25f43e0c4a0a8c7d947
Teodor Ilie <teo.altum.quinque@gmail.com>
2022-03-25 13:58:26 -0400

    Added patient status page.

---
+++ Website/status_page.php
@@ -0,0 +1,66 @@
+<!-- Website to display patient vaccination status. -->
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Patient Status</title>
+  <link rel="stylesheet" href="covid_style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Roboto:wght@100&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php
+include 'connect_covid_db.php';
+?>
+<h1>Patient Status</h1>
+<hr>
+
+<!-- Back to homepage button -->
+<form action="covid.php" method="post">
+
+  <input type="submit" value="Back to homepage">
+</form>
+
+<!-- List patient info -->
+<table>
+<?php
+   $patient_OHIP_val = "";
+   $which_patient= $_POST["patient_name"];
+   echo "<h2> Selected patient: " . $which_patient . "</h2>";
+   echo "<h3> Patient information: </h3>";
+   echo "<tr><th> First name </th><th> Last name </th><th> OHIP Number </th><th> Birthdate </th></tr>";
+
+   $query = 'SELECT * FROM `Patient` WHERE CONCAT(First_name, " ", Last_name) = "' . $which_patient . '"';
+   $result=$connection->query($query);
+    while ($row=$result->fetch()) {
+	     echo "<tr><td>" .$row["First_name"]."</td><td>".$row["Last_name"]."</td><td>".$row["OHIP_Number"]."</td><td>".$row["Birthdate"]."</td></tr>";
+       $patient_OHIP_val = $row["OHIP_Number"];
+    }
+?>
+</table>
+
+<table>
+<?php
+// List vaccine info for patient in new table
+
+  echo "<h3> Patient vaccinations records: </h3>";
+  echo "<tr><th> Vaccination date </th><th> Vaccine type </th></tr>";
+
+  $query = 'SELECT Vaccination_date, Producer_name FROM Recieves_vaccine as r, Vaccine_lot as v WHERE r.Lot_number = v.Lot_number and Patient_OHIP = "' . $patient_OHIP_val . '" ORDER BY Vaccination_date DESC';
+  $result=$connection->query($query);
+   while ($row=$result->fetch()) {
+      echo "<tr><td>" .$row["Vaccination_date"]."</td><td>".$row["Producer_name"]."</td></tr>";
+   }
+?>
+</table>
+<br>
+<body> Note: Most recent immunizations are shown first. </body>
+
+<?php
+   $connection = NULL;
+?>
+</body>
+</html>
