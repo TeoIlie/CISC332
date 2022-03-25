cbb6b36e9e13057fff4eab8cdc0673b1a1e3c426
Teodor Ilie <teo.altum.quinque@gmail.com>
2022-03-25 14:02:26 -0400

    Update workers_page.php

--- Website/workers_page.php
+++ Website/workers_page.php
@@ -6,6 +6,10 @@
   <meta charset="utf-8">
   <title>Site Workers</title>
   <link rel="stylesheet" href="covid_style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Roboto:wght@100&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php
@@ -25,7 +29,7 @@ include 'connect_covid_db.php';
 <?php
    $which_site= $_POST["vaccine_site"];
    echo "<h2>Selected site: " . $which_site . "</h2>";
-
+   echo "<tr><th> First name </th><th> Last name </th></tr>";
 
    echo "<h3>Doctors:</h3>";
    $query = 'SELECT First_name, Last_name FROM Doctor as d, Doctor_staffs as ds, Vaccine_site as v WHERE d.Unique_ID = ds.Doctor_ID and ds.Vaccine_site_name = v.Name and v.Name = "' . $which_site . '"';
@@ -43,6 +47,7 @@ include 'connect_covid_db.php';
    $which_site= $_POST["vaccine_site"];
 
    echo "<h3>Nurses:</h3>";
+   echo "<tr><th> First name </th><th> Last name </th></tr>";
    $query = 'SELECT First_name, Last_name FROM Nurse as n, Nurse_staffs as ns, Vaccine_site as v WHERE n.Unique_ID = ns.Nurse_ID and ns.Vaccine_site_name = v.Name and v.Name = "' . $which_site . '"';
    $result=$connection->query($query);
     while ($row=$result->fetch()) {
