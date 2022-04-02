f3c3982ee62b12b3d7d2f4159c187b0c0842a866
Teodor Ilie <teo.altum.quinque@gmail.com>
2022-04-01 21:28:14 -0400

    Update covid.php

--- Website/covid.php
+++ Website/covid.php
@@ -13,6 +13,7 @@
 </head>
 
 <!-- The body information -->
+<div class="homepage">
 <body>
   <h1>🦠 Covid Database</h1>
   <hr>
@@ -21,44 +22,70 @@
   include 'connect_covid_db.php';
   ?>
 
-  <!-- Find workers by vaccine site -->
-  <h2>Search workers by vaccine site</h2>
-  <form action="workers_page.php" method="post">
+  <div class="row">
+    <div class="column">
+      <!-- Enter information for a new or existing patient vaccination -->
+      <h2>Add vaccination record</h2>
+      <form action="covid.php" id="#form" method="post" name="#form">
+        <label><h3>Enter OHIP Number:<h3></label>
+        <input id="OHIP" name="OHIP" placeholder='OHIP' type='text' value="">
+        <input id='btn' name="submit" type='submit' value='Submit'>
 
-    <?php
-    include 'get_site_data.php';
-    ?>
+        <?php
+        include "redirect.php";
+        ?>
 
-    <input type="submit" value="List health workers">
-  </form>
+      </form>
+    </div>
+
+    <div class="column">
+      <!-- Show vaccine status of a patient -->
+      <h2>Search vaccination status by patient name</h2>
+      <form action="status_page.php" method="post">
+
+        <?php
+        include 'get_patient_data.php';
+        ?>
+
+        <input type="submit" value="Get vaccination status">
+      </form>
+    </div>
+  </div>
   <hr>
 
-  <!-- Find vaccine sites by vaccine type -->
-  <h2>Search vaccine sites by vaccine type</h2>
-  <form action="sites_page.php" method="post">
+  <div class="row">
+    <div class="column">
+      <!-- Find workers by vaccine site -->
+      <h2>Search workers by vaccine site</h2>
+      <form action="workers_page.php" method="post">
 
-    <?php
-    include 'get_vaccine_data.php';
-    ?>
+        <?php
+        include 'get_site_data.php';
+        ?>
 
-    <input type="submit" value="List vaccine sites">
-  </form>
+        <input type="submit" value="List health workers">
+      </form>
+    </div>
+
+    <div class="column">
+      <!-- Find vaccine sites by vaccine type -->
+      <h2>Search vaccine sites by vaccine type</h2>
+      <form action="sites_page.php" method="post">
+
+        <?php
+        include 'get_vaccine_data.php';
+        ?>
+
+        <input type="submit" value="List vaccine sites">
+      </form>
+    </div>
+  </div>
   <hr>
-
-  <!-- Show vaccine status of a patient -->
-  <h2>Find vaccination status by patient name</h2>
-  <form action="status_page.php" method="post">
-
-    <?php
-    include 'get_patient_data.php';
-    ?>
-
-    <input type="submit" value="Get vaccination status">
-  </form>
 
   <?php
   $connection =- NULL;
   ?>
 
 </body>
+</div>
 </html>
